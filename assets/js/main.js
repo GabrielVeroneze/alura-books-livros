b1b85dbc928b3ef357623bcc6db519aa1150cce1
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2023-02-28 22:54:02 -0300

    :test_tube: test: Busca livros da API

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+let livros = []
+const endpointDaAPI = 'https://guilhermeonrails.github.io/casadocodigo/livros.json'
+
+buscarLivrosDaAPI ()
+
+async function buscarLivrosDaAPI() {
+    const res = await fetch(endpointDaAPI)
+    livros = await res.json()
+
+    console.table(livros)
+}
