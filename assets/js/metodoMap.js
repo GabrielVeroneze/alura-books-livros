82c83748aa43fd93c4b9a9103cbebf6e63de3daf
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2023-03-02 22:59:27 -0300

    :bulb: docs: Comentários

--- assets/js/metodoMap.js
+++ assets/js/metodoMap.js
@@ -1,9 +1,14 @@
 function aplicarDesconto(livros) {
     const desconto = 0.3
     
+    // O método map vai invocar a função callback para cada objeto do array livros
     const livrosClone = livros.map(livro => {
-        return {...livro, preco: livro.preco - (livro.preco * desconto)}
+        return {
+            ...livro,   // Faz uma cópia do objeto 'livro'
+            preco: livro.preco - (livro.preco * desconto)   // Altera o valor da chave 'preco'
+        }
     })
 
+    // Retorna um novo array de objetos com o desconto aplicado
     return livrosClone
 }
