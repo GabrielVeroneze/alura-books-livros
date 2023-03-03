0f017545db650dc074b3a3198f28213d09972c50
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2023-03-02 23:00:29 -0300

    :test_tube: test: Filtra categoria dos livros

---
+++ assets/js/metodoFilter.js
@@ -0,0 +1,7 @@
+const botaoFiltrarFront = document.querySelector('#botaoFiltrarFront')
+
+botaoFiltrarFront.addEventListener('click', filtrarLivrosFront)
+
+function filtrarLivrosFront() {
+    console.log('Filtrado!!!')
+}
