235563f73b109007ac0f76cd5c6ddea985df0b22
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2023-03-08 22:39:45 -0300

    :bulb: docs: Remove comentários

--- assets/js/metodoFilter.js
+++ assets/js/metodoFilter.js
@@ -1,16 +1,12 @@
 const botoes = document.querySelectorAll('.navegacao__botao')
 
 botoes.forEach(botao => {
-    // O evento de clique chama a função 'filtrarLivros' para cada botao
     botao.addEventListener('click', event => filtrarLivros(event.target.value))
 })
 
-// recebe como parâmetro o valor do botao clicado ('front-end', 'back-end', 'dados', 'disponivel')
 function filtrarLivros(categoria) {
-    // Se a categoria for igual 'disponivel' chama a função filtrarPorDisponibilidade(), senão, chama a função filtrarPorCategoria()  
     let livrosFiltrados = categoria == 'disponivel' ? filtrarPorDisponibilidade() : filtrarPorCategoria(categoria)
     
-    // Exibe os livros filtrados na tela
     exibirLivros(livrosFiltrados)
     
     if (categoria == 'disponivel') {
@@ -20,12 +16,10 @@ function filtrarLivros(categoria) {
 }
 
 function filtrarPorCategoria(categoria) {
-    // Retorna um novo array com os livros que possuir o valor da categoria igual o do parâmetro categoria
     return livros.filter(livro => livro.categoria === categoria)
 }
 
 function filtrarPorDisponibilidade() {
-    // Retorna um novo array com os livros que possuir a quantidade maior que zero
     return livros.filter(livro => livro.quantidade > 0)
 }
 
--- assets/js/metodoSort.js
+++ assets/js/metodoSort.js
@@ -3,14 +3,11 @@ const botaoOrdenarPreco = document.querySelector('#botaoOrdenarPreco')
 botaoOrdenarPreco.addEventListener('click', ordenarPrecoLivros)
 
 function ordenarPrecoLivros() {
-    // Cria uma copia do array de objetos 'livros' para que o método sort() não modifique o array original    
     const livrosClone = [...livros]
     
-    // Ordena de forma crescente os objetos de livrosClone de acordo com o valor da sua propriedade preco 
     const livrosOrdenados = livrosClone.sort((a, b) => {
         return a.preco - b.preco
     })
 
-    // Exibe na tela o os livros ordenados pelo preço
     exibirLivros(livrosOrdenados)
 }
